e1aa2209b0b634a172e00c8728f74c21b6d73754
caldeira04 <joao.p.caldeira@icloud.com>
2024-03-15 16:41:43 -0300

    Finished the "main" scoreboard functions. Todo: styling, tie-break system, optimizing.

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,63 @@
 <script setup>
-
+  import {ref} from "vue";
+  // Creation of all the variables needed. unfortunately, i don´t know yet how to optimize this :(
+  const score1 = ref(0)
+  const score2 = ref(0)
+  const game1 = ref(0)
+  const game2 = ref(0)
+  const set1 = ref(0)
+  const set2 = ref(0)
   
+  // Creation of the functions responsible for increasing the scores of each player
+  function increaseScore1() {
+    if (score1.value == 0) {
+      score1.value = 15
+      console.log("Score increased")
+    } else if (score1.value == 15) {
+      score1.value = 30
+      console.log("Score increased")
+    } else if (score1.value == 30) {
+      score1.value = 40
+      console.log("Score increased")
+    } else {
+      score1.value = 0
+      score2.value = 0
+      game1.value++
+      console.log("Score and game increased") // When a game is won, both the points must be returned to 0.
+    }
+    if (game1.value == 6) {
+      game1.value = 0
+      game2.value = 0
+      set1.value++
+      console.log("Game and Set increased") // Same as the points, both must be set to 0 again.
+    }
+  }
+
+  // Pretty much a copypaste from the above function. Anyway, if I figure out how to optimize it, I'm doin it.
+  function increaseScore2() {
+    if (score2.value == 0) {
+      score2.value = 15
+      console.log("Score increased")
+    } else if (score2.value == 15) {
+      score2.value = 30
+      console.log("Score increased")
+    } else if (score2.value == 30) {
+      score2.value = 40
+      console.log("Score increased")
+    } else {
+      score2.value = 0
+      score1.value = 0
+      game2.value++
+      console.log("Score and game increased") // When a game is won, both the points must be returned to 0.
+    }
+    if (game2.value == 6) {
+      game2.value = 0
+      game1.value = 0
+      set2.value++
+      console.log("Game and Set increased") // Same as the points, both must be set to 0 again.
+    }
+  }
+
 </script>
 
 
@@ -10,17 +67,17 @@
       <table>
         <tr> <!-- First double scoreboard -->
           <td>COE/TAP</td>
-          <td>7</td>
-          <td>0</td>
-          <td>15</td>
-          <td><button>+</button></td>
+          <td>{{ set1 }}</td>
+          <td>{{ game1 }}</td>
+          <td>{{ score1 }}</td>
+          <td><button @click="increaseScore1()">+</button></td>
         </tr>
         <tr> <!-- Second double scoreboard -->
           <td>DIN/STU</td>
-          <td>6</td>
-          <td>0</td>
-          <td>40</td>
-          <td><button>+</button></td>
+          <td>{{ set2 }}</td>
+          <td>{{ game2 }}</td>
+          <td>{{ score2 }}</td>
+          <td><button @click="increaseScore2()">+</button></td>
         </tr>
       </table>
     </div>
