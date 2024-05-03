573e77cbf083a0fbd0663d786d1375941d7c8d6c
caldeira04 <joao.p.caldeira@icloud.com>
2024-05-03 01:28:04 -0300

    Finished most of the customization part. Still todo: 1. changing the player's names; 2. Separating the two screens (set-up and playing)

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup>
-  import {onMounted, ref, watch} from "vue";
+
+  import {ref} from "vue";
 
   // Creation of all the variables needed. Unfortunately, i don´t know yet how to optimize this :(
   const score1 = ref(0)
@@ -8,29 +9,28 @@
   const game2 = ref(0)
   const set1 = ref(0)
   const set2 = ref(0)
-  const maxGames = ref(6)
+  const maxSets = ref(0)
+  const maxGames = ref(0)
+  const maxTieBreak = ref(0)
   let tieBreak = false
-  
-  onMounted(() => {
-    startGame()
-  })
-  
-  
+  let softTie = false
+  let isReady = false
+
 // Declarations of the methods used for most of the program functionalities.
 
   // Function responsible for resetting the settings on the game when a new game is started.
   function startGame() {
     console.clear()
     console.log("Game started")
+    isReady = true
     tieBreak = false
-    maxGames.value = 6
     score1.value = 0
     score2.value = 0
     game1.value = 0
     game2.value = 0
     set1.value = 0
     set2.value = 0
-
+    alert(`Número de sets: ${maxSets.value}, número de games: ${maxGames.value}, máximo do tie-break: ${maxTieBreak.value}`)
   }
 
   // Function responsible for increasing the game (found the optimization lol)
@@ -42,7 +42,7 @@
         game1.value++
         console.log("Game increased")
         break;
-      case 7:
+      case maxTieBreak.value:
         score1.value = 0
         score2.value = 0
         game1.value++
@@ -61,7 +61,7 @@
         game2.value++
         console.log("Game increased")
         break;
-      case 7:
+      case maxTieBreak.value:
         score1.value = 0
         score2.value = 0
         game2.value++
@@ -100,20 +100,25 @@
     }
   }
 
-  // Function responsible for checking if a game is tied at 5x5. If it is, automatically adjusts for the game to end on 7x5
+  // Function responsible for checking if a game is tied at maxScore. If it is, automatically adjusts for the game to end on 7x5
   function testForTie() {
-    if (game1.value == 5 && game2.value == 5) {
+    if (softTie == false && game1.value == game2.value && game1.value == maxGames.value - 1) {
+      softTie = true
       console.log("The game hit a soft tie and must end by a 2 game advantage or tie-break on 6-6")
-      maxGames.value = 7
+      maxGames.value++
     }
-    if (game1.value == 6 && game2.value == 6) {
-      console.log("The game is a tie and must end by a FT7 tie-break")
-      tieBreak = true
+
+    if (softTie) {
+      if (game1.value == game2.value && game1.value == maxGames.value - 1){
+        tieBreak = true
+        softTie = false
+      }
     }
-    if (score1.value == 7){
+
+    if (score1.value == maxTieBreak.value){
       increaseGame1()
     }
-    if (score2.value == 7){
+    if (score2.value == maxTieBreak.value){
       increaseGame2()
     }
   }
@@ -123,8 +128,9 @@
     switch (tieBreak) {
       case true: 
       switch (score1.value) {
-        case 7:
-          increaseGame1();
+        case maxTieBreak.value:
+          increaseSet1();
+          maxGames.value--
           break;
 
         default:
@@ -160,8 +166,9 @@
 
     increaseSet1()
 
-    if (set1.value == 2) {
-      console.log("Game finished. Player 1 won.")
+    if (set1.value == maxSets.value) {
+      alert("Game finished. Player 1 won.")
+      startGame();
     }
   }
   
@@ -170,8 +177,9 @@
     switch (tieBreak) {
       case true: 
         switch (score2.value) {
-          case 7:
-            increaseGame2();
+          case maxTieBreak.value:
+            increaseSet2();
+            maxGames.value--
             break;
           default: 
             score2.value++
@@ -203,8 +211,9 @@
 
     increaseSet2()
     
-    if (set2.value == 2) {
-      console.log("Game finished. Player 2 won.")
+    if (set2.value == maxSets.value) {
+      alert("Game finished. Player 2 won.")
+      startGame()
     }
     }
   
@@ -213,32 +222,52 @@
 
 <template>
   <main>
-    <div>
-      <!-- More info on the functions on the <script> section. -->
-      <button @click="startGame()">Start game</button>
-      <table>
-        <tr> <!-- First double scoreboard -->
-          <td class="name">COE/TAP</td>
-          <td class="num">{{ set1 }}</td>
-          <td class="num">{{ game1 }}</td>
-          <td class="num">{{ score1 }}</td>
-          <td class="num"><button class="score" @click="increaseScore1();">+</button></td>
-        </tr>
-        <tr> <!-- Second double scoreboard -->
-          <td class="name">DIN/STU</td>
-          <td class="num">{{ set2 }}</td>
-          <td class="num">{{ game2 }}</td>
-          <td class="num">{{ score2 }}</td>
-          <td class="num"><button class="score" @click="increaseScore2();">+</button></td>
-        </tr>
-      </table>
-    </div>
+    <form @submit.prevent="setParams">
+      <div>
+        <h1>Set-up</h1>
+        <p>
+          <label for="maxSets">Number of Sets</label>
+          <input type="number" name="maxSets" id="maxSets" v-model="maxSets">
+        </p>
+        <p>
+          <label for="maxGames">Number of Games</label>
+          <input type="number" name="maxGames" id="maxGames" v-model="maxGames">
+        </p>
+        <p>
+          <label for="maxTieBreak">Tie-break score</label>
+          <input type="number" name="maxTieBreak" id="maxTieBreak" v-model="maxTieBreak">
+        </p>
+        <p>
+          <input type="submit" value="Start Game" @click="startGame()">
+        </p>
+      </div>
+    </form>
+    
+      <div v-show="isReady">
+        <!-- More info on the functions on the <script> section. -->
+        <table>
+          <tr> <!-- First double scoreboard -->
+            <td class="name">COE/TAP</td>
+            <td class="num">{{ set1 }}</td>
+            <td class="num">{{ game1 }}</td>
+            <td class="num">{{ score1 }}</td>
+            <td class="num"><button class="score" @click="increaseScore1();">+</button></td>
+          </tr>
+          <tr> <!-- Second double scoreboard -->
+            <td class="name">DIN/STU</td>
+            <td class="num">{{ set2 }}</td>
+            <td class="num">{{ game2 }}</td>
+            <td class="num">{{ score2 }}</td>
+            <td class="num"><button class="score" @click="increaseScore2();">+</button></td>
+          </tr>
+        </table>
+      </div>
   </main>
 </template>
 
 <style scoped> /* Just some placeholder styling for it to not look so simple. */
   main {
-    font-family: monospace;
+    font-family: Arial, Helvetica, sans-serif;
     zoom: 175%;
   }
   .score {
